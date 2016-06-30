910e335afe3ea108dd884f8475e16aea9ece73c9
Eric Chu <eric.chu.56@gmail.com>
2016-06-30 12:52:22 -0400

    Move decoding / mapping functions into separate utils file
    These have been used in test.lua, but they'll be useful to use
    while debugging g2p as well.
    
    Three functions:
    a) Converting inputs one hot encoded into tensor back into words
    b) Converting targets one hot encoded as table back into phonemes
    c) Decoding activations into phonemes

--- test.lua
+++ test.lua
@@ -1,10 +1,12 @@
 -- Load a network and evaluate it on the test set
+
+require 'warp_ctc'
+
 require 'model'
 require 'model_google'
 
 require 'utils.lua_utils'
-
-require 'warp_ctc'
+require 'utils.g2p_utils'
 
 -------------------------------------------------------------------------------------------
 -- Setup
@@ -46,69 +48,6 @@ if opt.test_on_valid then
 else
 	batcher = get_batcher(dataset.test, opt.batchsize, num_graphemes)
 end
-
-------------------------------------------------------------------------------------------
--- Utilities to convert batcher encoded inputs and targets back into words and phonemes
-------------------------------------------------------------------------------------------
--- Convert input (word encoded as tensor) back to word so we can compare against predictions
--- batchsize x batch_seq_len x input_dim
-local function convert_inputs_to_words(inputs, sizes)
-	local words = {}
-	for i=1,inputs:size(1) do 								-- 1 to batchsize
-		local word = ''
-		local one_hot_word = inputs[{{i},{},{}}]			-- one hot encoded: 1 x batch_seq_len x input_dim
-		local maxs, indices = torch.max(one_hot_word, 3)
-		-- print(sizes)
-		-- print(i)
-		for j=1,sizes[i] do
-			local idx = indices[1][j][1]
-			word = word .. idx_to_grapheme[idx]
-		end
-		table.insert(words, word)
-	end
-	return words
-end
-
--- Convert target to phonemes so we can compare against predictions
--- tbl = {1: 16, 2: 12, 3: 20}
-local function convert_targets_to_phonemes(targets)
-	local phonemes = {}
-	for i, encoded_phonemes in ipairs(targets) do
-		local phoneme_seq = {}
-		for j=1, #encoded_phonemes do
-			local idx = encoded_phonemes[j]
-			table.insert(phoneme_seq, idx_to_phoneme[idx])
-		end
-		table.insert(phonemes, phoneme_seq)
-	end
-	return phonemes
-end
-
-
--- Decode predictions (tensor)
-local function convert_preds_to_phonemes(predictions, sizes)
-	local preds = {}
-
-	-- Pluck out most likely phonemes by argmaxing over activations
-	local maxs, indices = torch.max(predictions, 3)
-	for i=1,indices:size(2) do					-- 1 to batchsize
-		local cur_pred = {}
-		local prev_phoneme = '-'				-- To decode CTC, can't have same token twice in a row
-		for j=1,sizes[i] do
-			local idx = indices[j][i][1]
-			idx = idx - 1						-- warp-CTC indexing starts at 0 (where 0 is the blank character)
-			local phoneme = idx_to_phoneme[idx]
-			if phoneme ~= '-' and phoneme ~= prev_phoneme then
-				table.insert(cur_pred, phoneme)
-				prev_phoneme = phoneme
-			end
-		end
-		table.insert(preds, cur_pred)
-	end
-	return preds
-end
-
-
 
 ------------------------------------------------------------------------------------------
 -- Testing
@@ -154,15 +93,15 @@ for i=1,batcher:num_batches() do
 		sizes = sizes:cuda()
 	end
 
-	local words = convert_inputs_to_words(inputs, sizes)
-	local phonemes = convert_targets_to_phonemes(targets)
+	local words = convert_inputs_to_words(inputs, sizes, idx_to_grapheme)
+	local phonemes = convert_targets_to_phonemes(targets, idx_to_phoneme)
 
 	local predictions = model:forward({ inputs, sizes }) -- batch_seq_length x batch x output_dim
 	local cur_batchsize	= #targets											-- Last batch in epoch may be smaller than opt.batchsize
 	predictions = predictions:view(-1, cur_batchsize, num_phonemes + 1)		-- seq_length x batch x output_dim
 	
 	-- Now use above to construct each predicted sequence
-	local preds = convert_preds_to_phonemes(predictions, sizes)
+	local preds = convert_preds_to_phonemes(predictions, sizes, idx_to_phoneme)
 
  	local batch_num_correct, batch_num_wrong, batch_matches = unpack(check_wer(phonemes, preds))
 
