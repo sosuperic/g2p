e93c13d387ebf01b500ce304fc04c475709e2190
Eric Chu <eric.chu.56@gmail.com>
2016-06-30 12:28:20 -0400

    Cleanup and small modifications to test.lua
    1) Setting batchsize equal to batchsize during training not necessary
       anymore because it's no longer encoded in model (see earlier commit)
    2) Clarify CTC indexing. I thought I was poorly handling an off by one
       error earlier. After checking the warp-ctc tutorial doc, I realized
       the 0 index is reserved for the BLANK character. Also, since Lua
       is 1-index-based, we have to subtract by 1.
    3) Move the update of prev_phoneme to inside the if. This fixes the following
       case: AA,BLANK,AA would be decoded as AA,AA.
    4) Add command line
    5) Cleaner printing (join table of phonemes)

--- test.lua
+++ test.lua
@@ -10,37 +10,42 @@ require 'warp_ctc'
 -- Setup
 ------------------------------------------------------------------------------------------
 
--- local gpuid = -1
--- if gpuid >= 0 then
--- 	inputs = inputs:cuda()
--- 	sizes = sizes:cuda()
--- end
+cmd = torch.CmdLine()
+cmd:text()
+cmd:option('-gpuid', -1, 'ID of gpu to run on')
+cmd:option('-test_on_valid', false, 'Test on valid or test set')
+cmd:option('-batchsize', 8, 'Minibatch size')
+local opt = cmd:parse(arg)
 
--- local model = torch.load('models/2016_6_28___16_53_4/e50_0.0046.t7')
--- local model = torch.load('models/2016_6_28___19_48_26/e6_0.7585.t7')
+if opt.gpuid >= 0 then
+	require 'cunn'
+	require 'cutorch'
+	require 'cudnn'
+	cutorch.setDevice((3 - opt.gpuid) + 1)
+	cutorch.manualSeed(123)
+end
+
+print('Loading model')
 local model = torch.load('models/2016_6_28___21_47_24/e13_0.8734.t7')
 model:evaluate()
 
+print('Getting data')
 local dataset, grapheme_to_idx, phoneme_to_idx = unpack(require 'cmudict_data')
+local num_phonemes = size_of_table(phoneme_to_idx)
 local num_graphemes = size_of_table(grapheme_to_idx)
 local idx_to_grapheme = invert_table(grapheme_to_idx)
--- print(size_of_table(phoneme_to_idx))
 
--- Add blank symbol for CTC
-phoneme_to_idx['-'] = 0		-- TODO: off by one... this fixes it but hmm..
+-- Add blank symbol for CTC (0 is reserved for blank, see warp-ctc tutorial)
+phoneme_to_idx['-'] = 0
 local idx_to_phoneme = invert_table(phoneme_to_idx)
 
--- print(idx_to_phoneme)
--- os.exit()
--- print(idx_to_phoneme)
-
-local batcher = require 'batcher'
-local batchsize = 8		-- This has to be the same as the original model
--- local batcher = get_batcher(dataset.valid, batchsize, num_graphemes)
-local batcher = get_batcher(dataset.test, batchsize, num_graphemes)
-
-local get_decoder = require 'decoder'
-local decoder = get_decoder(grapheme_to_idx, phoneme_to_idx)
+local get_batcher = require 'batcher'
+local batcher = nil
+if opt.test_on_valid then
+	batcher = get_batcher(dataset.valid, opt.batchsize, num_graphemes)
+else
+	batcher = get_batcher(dataset.test, opt.batchsize, num_graphemes)
+end
 
 ------------------------------------------------------------------------------------------
 -- Utilities to convert batcher encoded inputs and targets back into words and phonemes
@@ -79,30 +84,24 @@ local function convert_targets_to_phonemes(targets)
 	return phonemes
 end
 
--- TODO: Off by one... not sure why this is the case. See Evaluator in CTC as well
-	-- See TODO in this function, as well as TODO in 
-	-- This is handled, but not elegantly. what's the root cause?
--- Decode predictions (tensor: 
--- If argmax is highest idx, this corresponds to blank variable in CTC, therefore skip
+
+-- Decode predictions (tensor)
 local function convert_preds_to_phonemes(predictions, sizes)
 	local preds = {}
 
 	-- Pluck out most likely phonemes by argmaxing over activations
 	local maxs, indices = torch.max(predictions, 3)
-	for i=1,batchsize do
+	for i=1,indices:size(2) do					-- 1 to batchsize
 		local cur_pred = {}
 		local prev_phoneme = '-'				-- To decode CTC, can't have same token twice in a row
-		for j=1, sizes[i] do
+		for j=1,sizes[i] do
 			local idx = indices[j][i][1]
-			
-			-- HERE's the FIX:
-			idx = idx - 1
-
+			idx = idx - 1						-- warp-CTC indexing starts at 0 (where 0 is the blank character)
 			local phoneme = idx_to_phoneme[idx]
 			if phoneme ~= '-' and phoneme ~= prev_phoneme then
 				table.insert(cur_pred, phoneme)
+				prev_phoneme = phoneme
 			end
-			prev_phoneme = phoneme
 		end
 		table.insert(preds, cur_pred)
 	end
@@ -118,24 +117,29 @@ end
 -- Return number correct and number incorrect
 local function check_wer(phonemes, preds)
 	local num_correct, num_wrong = 0, 0
+	local matches = {}
+
 	for i=1,#phonemes do
-		if #phonemes[i] ~= #preds[i] then	-- Short circuit. Must have same number of phonemes obv
-			num_wrong = num_wrong + 1
+		local wrong = false
+		if #phonemes[i] ~= #preds[i] then	-- Short circuit. Must have same number of phonemes
+			wrong = true
 		else
-			local wrong = false
-			for j=1,#phonemes[i] do
+			for j=1,#phonemes[i] do 		-- Go through phoneme by phoneme
 				if phonemes[i][j] ~= preds[i][j] then
 					wrong = true
 				end
 			end
-			if wrong then
-				num_wrong = num_wrong + 1
-			else
-				num_correct = num_correct + 1
-			end
+		end
+
+		if wrong then
+			num_wrong = num_wrong + 1
+			table.insert(matches, 0)
+		else
+			num_correct = num_correct + 1
+			table.insert(matches, 1)
 		end
 	end
-	return {num_correct, num_wrong}
+	return {num_correct, num_wrong, matches}
 end
 
 ------------------------------------------------------------------------------------------
@@ -144,27 +148,32 @@ end
 local num_correct, num_wrong = 0, 0
 for i=1,batcher:num_batches() do
 	local inputs, targets, sizes = unpack(batcher:next_batch())
+	
+	if opt.gpuid >= 0 then
+		inputs = inputs:cuda()
+		sizes = sizes:cuda()
+	end
+
 	local words = convert_inputs_to_words(inputs, sizes)
 	local phonemes = convert_targets_to_phonemes(targets)
 
 	local predictions = model:forward({ inputs, sizes }) -- batch_seq_length x batch x output_dim
 	local cur_batchsize	= #targets											-- Last batch in epoch may be smaller than opt.batchsize
-	predictions = predictions:view(-1, cur_batchsize, num_phonemes + 1)		-- CTCCriterion expects seq_length x batch x output_dim
+	predictions = predictions:view(-1, cur_batchsize, num_phonemes + 1)		-- seq_length x batch x output_dim
 	
 	-- Now use above to construct each predicted sequence
 	local preds = convert_preds_to_phonemes(predictions, sizes)
 
+ 	local batch_num_correct, batch_num_wrong, batch_matches = unpack(check_wer(phonemes, preds))
+
  	-- Debugging: print original words, correct g2p, and predicted g2p 
- 	for i=1,batchsize do
- 		print(words[i])
- 		print(phonemes[i])
- 		print(preds[i])
+ 	for i=1,#targets do
+ 		print(batch_matches[i] .. ' ' .. words[i])
+ 		print(table.concat(phonemes[i], '-'))
+ 		print(table.concat(preds[i], '-'))
  	end
 
- 	local batch_num_correct, batch_num_wrong = unpack(check_wer(phonemes, preds))
  	num_correct = num_correct + batch_num_correct
  	num_wrong = num_wrong + batch_num_wrong 
  	print(num_correct, num_wrong, num_wrong / (num_correct + num_wrong))
 end
-
-print(num_correct, num_wrong, num_wrong / (num_correct + num_wrong))
